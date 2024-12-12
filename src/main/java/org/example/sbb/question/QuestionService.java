f44032a42dc324af7b86a71422e3ab0f1b133532
tigerpoint123 <tigerrla@naver.com>
2024-12-12 16:49:20 +0900

    jts chapter ~ 3-4

--- src/main/java/org/example/sbb/question/QuestionService.java
+++ src/main/java/org/example/sbb/question/QuestionService.java
@@ -2,9 +2,14 @@ package org.example.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.example.sbb.DataNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,10 +18,6 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-
-    public List<Question> getList() {
-        return questionRepository.findAll();
-    }
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = questionRepository.findById(id);
@@ -30,5 +31,12 @@ public class QuestionService {
         question.setContent(content);
         question.setCreateDate(LocalDateTime.now());
         questionRepository.save(question);
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAll(pageable);
     }
 }
