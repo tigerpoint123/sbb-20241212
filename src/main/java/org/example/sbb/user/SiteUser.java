d694239a19a18852b0d14844a74241a8cc9d34ef
tigerpoint123 <tigerrla@naver.com>
2024-12-13 10:25:50 +0900

    jts ~ 3-7 signup, login logout

---
+++ src/main/java/org/example/sbb/user/SiteUser.java
@@ -0,0 +1,23 @@
+package org.example.sbb.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
