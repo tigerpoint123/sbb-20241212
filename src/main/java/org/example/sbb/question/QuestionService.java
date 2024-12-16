8b7ebba1a9137dd01915b1af82f6fbcd23006f60
tigerpoint123 <tigerrla@naver.com>
2024-12-16 09:48:23 +0900

    jts ~ 3-13 search

--- src/main/java/org/example/sbb/question/QuestionService.java
+++ src/main/java/org/example/sbb/question/QuestionService.java
@@ -1,12 +1,15 @@
 package org.example.sbb.question;
 
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.example.sbb.DataNotFoundException;
+import org.example.sbb.answer.Answer;
 import org.example.sbb.user.SiteUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,7 +25,7 @@ public class QuestionService {
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = questionRepository.findById(id);
-        if(question.isPresent()) return question.get();
+        if (question.isPresent()) return question.get();
         else throw new DataNotFoundException("Question not found");
     }
 
@@ -35,11 +38,12 @@ public class QuestionService {
         questionRepository.save(question);
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return questionRepository.findAll(spec, pageable);
     }
 
     public void modify(Question question, String subject, String content) {
@@ -56,6 +60,28 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         questionRepository.save(question);
+    }
+
+    // 3-13 JPA의 Specification 인터페이스 사용하기
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
     }
 
 }
