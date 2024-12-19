1780db7ae69bb2d82a1748ac7a6071184c1d8922
tigerpoint123 <tigerrla@naver.com>
2024-12-19 16:12:15 +0900

    rollback code, reset db

--- src/test/java/org/example/sbb/SbbApplicationTests.java
+++ src/test/java/org/example/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package org.example.sbb;
 
+import org.example.sbb.category.Category;
 import org.example.sbb.question.QuestionService;
 import org.example.sbb.user.SiteUser;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,16 @@ class SbbApplicationTests {
 
     @Test
     void 데이터주입() {
+        SiteUser siteUser = new SiteUser();
+        Category category = new Category();
+        siteUser.setId(1);
+        category.setCategoryName("질문게시판");
+        category.setCategoryName("자유게시판");
+
         for (int i = 1; i <= 300; i++) {
             String subject = String.format("테스트 데이터입니다:[%03d]", i);
             String content = "내용무";
-            SiteUser siteUser = new SiteUser();
-            siteUser.setId(i);
-
+            questionService.create(subject, content, siteUser, category);
         }
     }
 }
